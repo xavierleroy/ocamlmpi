b69319343b2a80269779beace352c7f77755802b
Xavier Leroy <xavier.leroy@college-de-france.fr>
2024-03-07 15:46:44 +0100

    Don't install an error handler that raises an OCaml exception
    MPI errors are raised by MPI functions that are often within blocking sections
    (e.g. MPI_Send in caml_mpi_send).
    
    It's illegal to raise an OCaml exception from within a blocking section.
    OCaml 5.1 reports a fatal error.

--- init.c
+++ init.c
@@ -27,22 +27,6 @@
 
 static const value * caml_mpi_exn = NULL;
 
-static void caml_mpi_error_handler(MPI_Comm * comm, int * errcode, ...)
-{
-  char errmsg[MPI_MAX_ERROR_STRING + 1];
-  int resultlen;
-  value msg;
-
-  MPI_Error_string(*errcode, errmsg, &resultlen);
-  msg = caml_copy_string(errmsg);
-  if (caml_mpi_exn == NULL) {
-    caml_mpi_exn = caml_named_value("Mpi.Error");
-    if (caml_mpi_exn == NULL)
-      caml_invalid_argument("Exception MPI.Error not initialized");
-  }
-  caml_raise_with_arg(*caml_mpi_exn, msg);
-}
-
 void caml_mpi_raise_error(const char *msg)
 {
   if (caml_mpi_exn == NULL) {
@@ -71,23 +55,12 @@ value caml_mpi_init(value arguments)
 {
   int argc, i;
   char ** argv;
-  MPI_Errhandler hdlr;
 
   argc = Wosize_val(arguments);
   argv = caml_stat_alloc((argc + 1) * sizeof(char *));
   for (i = 0; i < argc; i++) argv[i] = Bp_val(Field(arguments, i));
   argv[i] = NULL;
   MPI_Init(&argc, &argv);
-  /* Register an error handler */
-#if MPI_VERSION >= 2
-  MPI_Comm_create_errhandler(
-	  (MPI_Handler_function *)caml_mpi_error_handler, &hdlr);
-  MPI_Comm_set_errhandler(MPI_COMM_WORLD, hdlr);
-#else
-  MPI_Errhandler_create((MPI_Handler_function *)caml_mpi_error_handler, &hdlr);
-  MPI_Errhandler_set(MPI_COMM_WORLD, hdlr);
-#endif
-
   return Val_unit;
 }
 
@@ -101,4 +74,3 @@ value caml_mpi_wtime(value unit)
 {
   return caml_copy_double(MPI_Wtime());
 }
-
